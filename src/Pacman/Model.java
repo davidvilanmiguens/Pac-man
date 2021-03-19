0669317fffcd93349c539e29751cce1a99c4c6ad
David Vilan Miguens <dvialnmiguens@danielcastelao.org>
2021-03-19 16:10:54 +0100

    2

--- src/Pacman/Model.java
+++ src/Pacman/Model.java
@@ -2,7 +2,7 @@ package Pacman;
 
 
 
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +12,8 @@ public class Model extends JPanel implements ActionListener {
 
     private Dimension d;
     private final Font smallFont= new Font("Arial", Font.BOLD,14);
-    private boolean inGame= false;
+    private boolean inGame= false;/** **/
+
     private boolean dying= false;
 
     private final int BLOCK_SIZE= 24;
@@ -28,6 +29,20 @@ public class Model extends JPanel implements ActionListener {
 
     private Image heart, ghost;
     private Image up,down,left,right;
+
+    private int pacman_x, pacman_y, pacmand_x, pacmand_y;
+    private int req_dx, req_dy;
+
+    private final int validSpeeds[]={1,2,3,4,6,8};
+    private final int maxSped=6;
+    private int currentSpeed=3;
+    private short [] screenData;
+    private Timer timer;
+
+    private final short leveData[]={
+
+    };
+
 
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
